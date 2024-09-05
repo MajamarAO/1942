a6be40935815145ed584b598b9fc9e36ebf20a4f
Galoberlini <110853629+Galoberlini@users.noreply.github.com>
2024-09-05 14:45:10 -0300

    Update test2.asm

--- Información_proyecto/tests/test2.asm
+++ Información_proyecto/tests/test2.asm
@@ -1,55 +1,58 @@
     .ORIG    x3000
 
 EXAMPLE_RND
-LD R0, EXAMPLE
-LD R1, EXAMPLE
-LD R2, ANCHO_EX
-LD R3, ALTO_EX
-LD R4, SPRITE_EX
+LEA R0, EXAMPLE
+LDR R1,R0, #0
+LDR R2,R0, #1
+LDR R3,R0, #2
+LDR R4,R0, #3
+LEA R5, EXAMPLE
+ADD R5, R5, #3
 LD R6, ROW
  
 RENDER
 STR R4,R1, #0
-ADD R4,R4, #1
+ADD R5,R5, #1
+LDR R4,R5, #0
 ADD R1,R1, #1
 ADD R2,R2, #-1
 BRp RENDER
-LD R2, ANCHO_EX
-NOT R5, R2
-ADD R5, R5, #1
-ADD R1, R1, R5
-ADD R1, R1, R6
+LDR R2, R0, #1 
+NOT R6, R2
+ADD R6, R6, #1
+ADD R1, R1, R
+ADD R1, R1, R7
 ADD R3,R3, #-1 
 BRp RENDER
 HALT
 
 EXAMPLE  .FILL xDD42
-ANCHO_EX .FILL 5
-ALTO_EX  .FILL 4
+         .FILL 5
+         .FILL 4
       
-SPRITE_EX .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
 
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
 
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
 
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
-          .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
+         .FILL x001F
         
 
 ROW     .FILL x0080
