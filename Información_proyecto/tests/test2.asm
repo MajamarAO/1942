7a9ac7c750f6d5e8247d89e0ca0b0a034d7380e7
Estudiantes <galoberlini@gmail.com>
2024-09-06 17:06:41 -0300

    Render casi terminado y avance movimiento

--- Información_proyecto/tests/test2.asm
+++ Información_proyecto/tests/test2.asm
@@ -7,24 +7,30 @@ LDR R2,R0, #1
 LDR R3,R0, #2
 LDR R4,R0, #3
 LEA R5, EXAMPLE
-ADD R5, R5, #3
-LD R6, ROW
+ADD R5, R5, #2
+LD R7, ROW
  
 RENDER
-STR R4,R1, #0
 ADD R5,R5, #1
 LDR R4,R5, #0
+BRn CHECK_INVIS
+STR R4,R1, #0
+CHECK_INVIS
 ADD R1,R1, #1
 ADD R2,R2, #-1
 BRp RENDER
 LDR R2, R0, #1 
 NOT R6, R2
 ADD R6, R6, #1
-ADD R1, R1, R
+ADD R1, R1, R6
 ADD R1, R1, R7
 ADD R3,R3, #-1 
 BRp RENDER
 HALT
+
+
+
+
 
 EXAMPLE  .FILL xDD42
          .FILL 5
@@ -36,23 +42,23 @@ EXAMPLE  .FILL xDD42
          .FILL x001F
          .FILL x001F
 
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
 
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
+         .FILL xF000
+         .FILL xF000
+         .FILL xF000
+         .FILL xF000
+         .FILL xF000
 
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
-         .FILL x001F
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
+         .FILL x7FFF
         
 
 ROW     .FILL x0080
