b889015a2c02c6ea28c78423854dbcf9ebba543e
alexcastr04 <164074588+alexcastr04@users.noreply.github.com>
2024-11-06 11:15:32 -0300

    Update Unrenderer.asm

--- Scripts/Unrenderer.asm
+++ Scripts/Unrenderer.asm
@@ -19,34 +19,37 @@
 ;R6 = NOT usado durante la operacion
 ;R7 = Columna
 
-UNRENDER
-LD R6, ROW
-LD R5, BLACK
-STR R5,R3, #0
-ADD R1,R1, #-1
-BRp UNRENDER
-ADD R3, R3, R6
-LDR R1, R0, #0
-NOT R6, R1
+    ;funcion para "borrar" un sprite (pinta de negro cada pixel tomando la direccion inicial del sprite R3)
+UNRENDER                          
+LD R6, ROW                        ;carga tamaño de la fila
+LD R5, BLACK                      ;carga el color negro (valor BLACK)
+STR R5,R3, #0                     ;"pinta" de negro en la posicion de R3
+ADD R1,R1, #-1                    ;disminuye el ancho (R1) en 1 para avanzar en fila
+BRp UNRENDER                      ;si aun hay columnas restantes (R1 > 0), vuelve a UNRENDER
+    ;salto a siguiente fila
+ADD R3, R3, R6                    ;aumenta R3 al tamaño de la fila
+LDR R1, R0, #0                    ;carga el ancho original 
+    ;ajustar alto
+NOT R6, R1                       ;calcula -R1
 ADD R6, R6, #1
 ADD R3, R3, R6
-ADD R2,R2, #-1 
-BRp UNRENDER
+ADD R2,R2, #-1                    ;disminuye el alto en 1
+BRp UNRENDER                      ;si aun hay fila restantes (R2 > 0) vuelve a UNRENDER
 
 
-PANTALLA     .FILL xC000
-ROW          .FILL x0080
-BLACK        .FILL x0000
+PANTALLA     .FILL xC000          ;valor direccion de la pantalla
+ROW          .FILL x0080          ;valor de desplazamiento por fila
+BLACK        .FILL x0000          ;valor color negro
 
 FIRST_PIXEL
-LD R3, PANTALLA
-LD R6, ROW
-ADD R3, R3, R1
-ADD R2, R2, #0
-BRz END_FSTPXL
+LD R3, PANTALLA                   ;carga la direcion base de la pantalla
+LD R6, ROW                        ;carga el tamaño de la fila
+ADD R3, R3, R1                    ;ajusta el ancho segun R1
+ADD R2, R2, #0                    ;carag cero (0) para verificar
+BRz END_FSTPXL                    ;si R2 es cero (0) entonces la altura termino y salta a END_FSTPXL
 SUM_ALTO
-ADD R3, R3, R6
-ADD R2, R2, #-1
-BRp SUM_ALTO
+ADD R3, R3, R6                    ;avanza una fila completa
+ADD R2, R2, #-1                   ;disminuye la altura en 1 
+BRp SUM_ALTO                      ;si la altura no llego a 0 (R2 > 0) vuelve a SUM_ALTO
 END_FSTPXL
 ret
