0b651cc67bfa2e4f316f43faa60902dae68e1674
alexcastr04 <164074588+alexcastr04@users.noreply.github.com>
2024-11-06 10:35:45 -0300

    Update Renderer.asm

--- Scripts/Renderer.asm
+++ Scripts/Renderer.asm
@@ -3,7 +3,8 @@
 ;tiene las coordenadas (x,y) siendo X el número de la fila e Y el número de la columna
 ;Para calcular el espacio en memoria que ocuparía se hace (xC000 + (X*x0080) + Y)
 
-;Funciones a hacer dentro de este archivo RENDER,CHECK_POS 
+;Funciones a hacer dentro de este archivo: RENDER,CHECK_POS 
+;funciones hechas: RENDER, CHECK_INVIS
 
 ;Intento de implementacion numero 1: El sprite estara cargado en memoria y tendra:
 ;1- direccion en memoria del pixel inicial
@@ -19,54 +20,59 @@
 ;R6 = NOT usado durante la operacion
 ;R7 = Columna
  
-
+  ;funcion para dibujar un sprite en pantalla
 RENDER
-LD R6, ROW
-ADD R4,R4, #1
-LDR R5,R4, #0
-BRn CHECK_INVIS
-STR R5,R3, #0
-CHECK_INVIS
-ADD R3,R3, #1
-ADD R1,R1, #-1
-BRp RENDER
-ADD R3, R3, R6
-LDR R1, R0, #0
-NOT R6, R1
-ADD R6, R6, #1
-ADD R3, R3, R6
-ADD R2,R2, #-1 
-BRp RENDER
+LD R6, ROW          ;carga en R6 valor desplazamiento por fila
+ADD R4,R4, #1       ;incrementa R4 para avanzar al siguiente pixel
+LDR R5,R4, #0       ;craga el valor actual del pixel desde R4
+BRn CHECK_INVIS     ;R5 valor negativo, salta a CHECK_INVIS para omitir pixel
+STR R5,R3, #0       ;R5 no negativo, se almacena en panatlla en R3
 
+CHECK_INVIS         
+ADD R3,R3, #1       ;aumenta R3 en 1 para avanzar a la siguient direccion en pantalla
+ADD R1,R1, #-1      ;disminuye R1 (ancho spirte) en 1 
+BRp RENDER          ;si quedan columnas (R1 > 0) vuelve a RENDER
+
+ADD R3, R3, R6      ;Avanza una fila completa (incrementa R3 en ROW)
+LDR R1, R0, #0      ;restaura valor original del ancho (desde R0)
+NOT R6, R1          ;calcula el complemento de R1 y lo almacena en R6
+ADD R6, R6, #1      ;aumenta R6 en 1 (realiza la operacion de -R1)
+ADD R3, R3, R6      ;ajusta R3 para alinear la dirección de inicio de la siguiente fila en pantalla
+ADD R2,R2, #-1      ;disminuye la altura del sprite
+BRp RENDER          ;si quedan filas (R2 > 0) vuelve al inicio de RENDER
+
+  ;funcion para borrar un sprite 
 UNRENDER
-LD R6, ROW
-LD R5, BLACK
-STR R5,R3, #0
-ADD R3, R3, #1
-ADD R1, R1, #-1
-BRp UNRENDER
-ADD R3, R3, R6
-LDR R1, R0, #0
-NOT R6, R1
-ADD R6, R6, #1
-ADD R3, R3, R6
-ADD R2,R2, #-1 
-BRp UNRENDER
-ret
+LD R6, ROW          ;carga en R6 valor desplazamiento por fila
+LD R5, BLACK        ;carga en R5 el color negro (BLACK) para "borrar" un pixel
+STR R5,R3, #0       ;almacena el valor de R5 (negro) en la pantalla en la dirección indicada por R3
+ADD R3, R3, #1      ;aumenta R3 en 1 para avanzar a la siguiente direccion en pantalla
+ADD R1, R1, #-1     ;disminuye R1 (ancho sprite) en 1
+BRp UNRENDER        ;si quedan columnas por borrar en la fila (R1 > 0), vuelve a UNRENDER
 
-PANTALLA      .FILL xC000
-ROW           .FILL x0080
-BLACK         .FILL x0000
+ADD R3, R3, R6      ;avanza una fila completa (incrementa R3 en ROW)
+LDR R1, R0, #0      ;restaura valor original del ancho (desde R0)
+NOT R6, R1          ;Calcula el complemento de R1 y lo almacena en R6
+ADD R6, R6, #1      ;aumenta R6 en 1 (esto efectúa la operación de -R1)
+ADD R3, R3, R6      ;ajusta R3 para alinear la dirección de inicio de la siguiente fila en pantalla
+ADD R2,R2, #-1      ;disminuye la altura del sprite (R2)
+BRp UNRENDER        ;Si quedan filas por borrar (R2 > 0), vuelve a UNRENDER
+ret                 ;Retorna del subprograma UNRENDER
 
+PANTALLA      .FILL xC000      ;direccion de la pantalla
+ROW           .FILL x0080      ;valor de desplazamiento por fila
+BLACK         .FILL x0000      ;valor color negro
+
+  ;funcion para apuntar al primer pixel donde comienza el dibujo (renderizado)
 FIRST_PIXEL
-LD R3, PANTALLA
-LD R6, ROW
-ADD R3, R3, R1
-ADD R2, R2, #0
-BRz END_FSTPXL
+LD R3, PANTALLA     ;carga el valor base de la pantalla
+LD R6, ROW          ;carga el desplazamiento por fila
+ADD R3, R3, R1      ;ajusta R3 para la posicion inicial del ancho
+ADD R2, R2, #0      ;añade 0 a R2 para verificar si es cero
+BRz END_FSTPXL      ;si la altura es 0 (R2), salta a END_FSTPXL
 SUM_ALTO
-ADD R3, R3, R6
-ADD R2, R2, #-1
-BRp SUM_ALTO
+ADD R3, R3, R6      ;avanza una fila completa (incrementa R3 en ROW)
+ADD R2, R2, #-1     ;decrementa R2 en 1 (proceso de ajuste en altura)
+BRp SUM_ALTO        ;si altura no llego a cero (R2 > 0), vuelve a SUM_ALTO
 END_FSTPXL
-ret
+ret                 ;retornar a FISRT_PIXEL
